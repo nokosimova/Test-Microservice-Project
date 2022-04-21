561cd0481462d86b1dab612f51e98d58d8b24523
Nodira Kosimova <nkosimova06@gmail.com>
2022-04-21 13:16:05 +0500

    add http client for command service

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(opt => 
     opt.UseInMemoryDatabase("InMemDb"));
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
-
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,35 @@
+using System.Text;
+using System.Text.Json;
+using PlatformService.DTOs;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public  async Task SendPlatformCommand(PlatformReadDto platform)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(platform),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}/api/c/platforms", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync Post to Command service was OK!");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync Post to Command service was NOT OK!");
+            }
+        }
+    }
+}
