0fba63429571248acca6fda8d33bacae1b860d71
Nodira Kosimova <nkosimova06@gmail.com>
2022-04-21 13:15:46 +0500

    create Command Service

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound Post # Command Service");
+            
+            return Ok("Inbound test of from Platform Controller");
+        }
+
+    }
+
+}
